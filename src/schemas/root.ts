c40fe890ef5e2993e61db5baf666f8b09b053eee
Henry Kellam <henry.a.kellam@gmail.com>
2022-03-24 21:23:45 -0400

    convert files to typescript

---
+++ src/schemas/root.ts
@@ -0,0 +1,22 @@
+import { gql } from 'apollo-server-lambda';
+
+const typeDefs = gql`
+  type Query {
+    healthcheck: Boolean
+  }
+
+  type Mutation {
+    ping: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    healthcheck: () => true
+  },
+  Mutation: {
+    ping: () => 'pong'
+  }
+};
+
+module.exports = { typeDefs, resolvers };
